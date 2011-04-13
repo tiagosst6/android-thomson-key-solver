09ab83e6fbc4ffeacbf61cc31e51353a04a3fe7d
ruka.araujo <ruka.araujo@gmail.com>
2011-04-13 23:27:33 +0000

    [Android] Updated donate.patch and added some wristles and bells to the script.

--- donate.sh
+++ donate.sh
@@ -2,25 +2,25 @@
 
 PATCH_FILE=donate.patch
 FOLDER=android/jni/include
-if [ "$1" == "-r" ]
+if [ "$1" == "-r" ] || [ "$1" == "-R" ]
 then
-	svn revert -R android/
-	rm $FOLDER/org_exobel_routerkeygen_donate_NativeThomson.h 
-	rm $FOLDER/org_exobel_routerkeygen_donate_ThomsonKeygen.h 
+	svn revert -R android/  2>&1
+	rm $FOLDER/org_exobel_routerkeygen_donate_NativeThomson.h   2>&1
+	rm $FOLDER/org_exobel_routerkeygen_donate_ThomsonKeygen.h   2>&1
 	echo "Changes reversed" 
 	exit
 fi
 
 if [ -f "$PATCH_FILE" ]
 then
-	patch -p0 < $PATCH_FILE
+	patch -f -p0 < $PATCH_FILE  2>&1
 else
 	echo "A patch file must be present!" 
 	exit
 fi
 
-mv $FOLDER/org_exobel_routerkeygen_NativeThomson.h $FOLDER/org_exobel_routerkeygen_donate_NativeThomson.h 
-mv $FOLDER/org_exobel_routerkeygen_ThomsonKeygen.h $FOLDER/org_exobel_routerkeygen_donate_ThomsonKeygen.h 
+mv $FOLDER/org_exobel_routerkeygen_NativeThomson.h $FOLDER/org_exobel_routerkeygen_donate_NativeThomson.h  2>&1
+mv $FOLDER/org_exobel_routerkeygen_ThomsonKeygen.h $FOLDER/org_exobel_routerkeygen_donate_ThomsonKeygen.h  2>&1
 export NDK_PROJECT_PATH=/home/ruka/Projectos/RouterKeygen/android/
 echo "Don't forget to invoke ndk-build now." 
 
